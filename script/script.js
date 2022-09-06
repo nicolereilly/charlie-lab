fe706ff8941dc4153794ac4f21845e1dcd18db51
nicolereilly <111534190+nicolereilly@users.noreply.github.com>
2022-09-06 14:36:26 -0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  //click on an image, get a message
+
+  $("#dogeOne").click(function () {
+        $("#outputDiv").hide();
+
+    console.log("user clicked dogeOne 2");
+    $("#outputMsg").text("How could you get this wrong ????!");
+    $("#outputDiv").toggle();
+  });
+
+  $("#dogeTwo").click(function () {
+    console.log("user clicked dogeTwo 2");
+        $("#outputMsg").text("Congrats!!!!!!!! your WOW is WOWing me!");
+    $("#outputDiv").toggle();
+  });
+});
